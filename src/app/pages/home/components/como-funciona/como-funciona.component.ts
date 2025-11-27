41ef2387456acbceb238d9be89e5e2b385d22d37
mateus-msantos-dev <mateus.msantos.dev@gmail.com>
2025-11-27 11:20:08 -0300

    finalizando o footer

---
+++ src/app/pages/home/components/como-funciona/como-funciona.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-como-funciona',
+  imports: [],
+  templateUrl: './como-funciona.component.html',
+  styleUrl: './como-funciona.component.css'
+})
+export class ComoFuncionaComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './components/hero/hero.component';
 import { CategoriasComponent } from './components/categorias/categorias.component';
 import { ProdutosPopularesComponent } from './components/produtos-populares/produtos-populares.component';
+import { ComoFuncionaComponent } from "./components/como-funciona/como-funciona.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, CategoriasComponent, ProdutosPopularesComponent],
+  imports: [HeroComponent, CategoriasComponent, ProdutosPopularesComponent, ComoFuncionaComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
