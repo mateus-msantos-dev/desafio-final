fd4fb770a0ecf00f9529e4854d43cd19a3cdd806
mateus-msantos-dev <mateus.msantos.dev@gmail.com>
2025-11-28 00:04:05 -0300

    Criando o header pra desktop e mobile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  menuAberto = false;
 
+  toggleMenu() {
+    this.menuAberto = !this.menuAberto;
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import { CategoriasComponent } from './components/categorias/categorias.componen
 import { ProdutosPopularesComponent } from './components/produtos-populares/produtos-populares.component';
 import { ComoFuncionaComponent } from "./components/como-funciona/como-funciona.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, CategoriasComponent, ProdutosPopularesComponent, ComoFuncionaComponent, FooterComponent],
+  imports: [HeroComponent, CategoriasComponent, ProdutosPopularesComponent, ComoFuncionaComponent, FooterComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
