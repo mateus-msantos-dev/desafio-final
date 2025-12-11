eddccd21a4a6889b0a27915d788a9a9189c90370
mateus-msantos-dev <mateus.msantos.dev@gmail.com>
2025-12-11 01:59:42 -0300

    Melhorando o footer e atualizando a estilização dos botões do carrinho

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  // Pega o ano atual automaticamente (ex: 2025)
+  currentYear: number = new Date().getFullYear();
 }
